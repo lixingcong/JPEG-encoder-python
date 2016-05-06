47f039372ac142b302d8387abae44d9046d143a7
lixingcong <lixingcong@live.com>
2016-05-06 21:55:27 +0800

    [block_split.py]: function prototype

---
+++ src/block_split.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Time-stamp: < block_split.py 2016-05-06 21:54:27 >
+"""
+块分割
+""" 
+
+import numpy as np
+import math
+
+def padding_dummy_edge(input_matrix):
+	pass
+
+def split_to_blocks(input_matrix):
+	pass
+
+def test():
+	pass
+
+if __name__ == '__main__':
+	test()
