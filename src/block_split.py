bbb4f123d1f1ae9072fca6b8fcf23d00d538b82f
lixingcong <lixingcong@live.com>
2016-05-07 00:21:30 +0800

    [block_split.py]: split blocks is ok

--- src/block_split.py
+++ src/block_split.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Time-stamp: < block_split.py 2016-05-06 23:36:17 >
+# Time-stamp: < block_split.py 2016-05-07 00:20:35 >
 """
 块分割
 """ 
 
 import numpy as np
 import math
+
+# 最终的方块边长
+blk_width=8
 
 # 测试矩阵，课本P129，已删除部份，留下6x7矩阵
 test_table=np.array([
@@ -18,13 +21,13 @@ test_table=np.array([
 	[161,161,161,161,160,157,157]
 ])
 
-def padding_dummy_edge(input_matrix):
+def padding_dummy_edge(input_matrix,blk_width_):
 	height=input_matrix.shape[0]
 	width=input_matrix.shape[1]
-	if height%8==0 and width%8==0:
+	if height%blk_width_==0 and width%blk_width_==0:
 		return input_matrix
-	height_new=height+(8-height%8)
-	width_new=width+(8-width%8)
+	height_new=height+(blk_width_-height%blk_width_)
+	width_new=width+(blk_width_-width%blk_width_)
 	new_table=np.zeros(height_new*width_new,
 					   dtype=np.float64).reshape(height_new,width_new)
 	for x,y in [(x,y) for x in xrange(height) for y in xrange(width)]:
@@ -36,16 +39,28 @@ def padding_dummy_edge(input_matrix):
 	for x in xrange(height,height_new):
 		for y in xrange(width_new):
 			new_table[x,y]=new_table[height-1,y]
-
-	print input_matrix
-	print '-'*10
-	print new_table
+	return new_table
 	
-def split_to_blocks(input_matrix):
-	pass
+def split_to_blocks(input_matrix,blk_width_):
+	height=input_matrix.shape[0]
+	width=input_matrix.shape[1]
+	horizontal_blocks_num=height/blk_width_
+	vertical_blocks_num=width/blk_width_
+	# 先横向分割，再纵向分割
+	vertical_blocks=np.vsplit(input_matrix,vertical_blocks_num)
+	for block_vert in vertical_blocks:
+		horizontal_blocks=np.hsplit(block_vert,horizontal_blocks_num)
+		for block in horizontal_blocks:
+			print block
+			print '-'*10
 
 def test():
-	padding_dummy_edge(test_table)
+	padded_table=padding_dummy_edge(test_table,8)
+	print "Original blocks:"
+	print padded_table
+	print '*'*10
+	print "cutted blocks:"
+	split_to_blocks(padded_table,4)
 
 if __name__ == '__main__':
 	test()
