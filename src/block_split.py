dad677b6adec5a4a6843178ba56ce13ff810faeb
lixingcong <lixingcong@live.com>
2016-05-07 00:28:47 +0800

    [block_split.py]: add all blocks to a list

--- src/block_split.py
+++ src/block_split.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Time-stamp: < block_split.py 2016-05-07 00:20:35 >
+# Time-stamp: < block_split.py 2016-05-07 00:28:12 >
 """
 块分割
 """ 
@@ -8,8 +8,8 @@
 import numpy as np
 import math
 
-# 最终的方块边长
-blk_width=8
+# 最终的方块边长，调试用。
+# blk_width=8
 
 # 测试矩阵，课本P129，已删除部份，留下6x7矩阵
 test_table=np.array([
@@ -21,13 +21,13 @@ test_table=np.array([
 	[161,161,161,161,160,157,157]
 ])
 
-def padding_dummy_edge(input_matrix,blk_width_):
+def padding_dummy_edge(input_matrix,blk_width):
 	height=input_matrix.shape[0]
 	width=input_matrix.shape[1]
-	if height%blk_width_==0 and width%blk_width_==0:
+	if height%blk_width==0 and width%blk_width==0:
 		return input_matrix
-	height_new=height+(blk_width_-height%blk_width_)
-	width_new=width+(blk_width_-width%blk_width_)
+	height_new=height+(blk_width-height%blk_width)
+	width_new=width+(blk_width-width%blk_width)
 	new_table=np.zeros(height_new*width_new,
 					   dtype=np.float64).reshape(height_new,width_new)
 	for x,y in [(x,y) for x in xrange(height) for y in xrange(width)]:
@@ -41,18 +41,19 @@ def padding_dummy_edge(input_matrix,blk_width_):
 			new_table[x,y]=new_table[height-1,y]
 	return new_table
 	
-def split_to_blocks(input_matrix,blk_width_):
+def split_to_blocks(input_matrix,blk_width):
 	height=input_matrix.shape[0]
 	width=input_matrix.shape[1]
-	horizontal_blocks_num=height/blk_width_
-	vertical_blocks_num=width/blk_width_
+	horizontal_blocks_num=height/blk_width
+	vertical_blocks_num=width/blk_width
 	# 先横向分割，再纵向分割
+	all_small_blocks=[]
 	vertical_blocks=np.vsplit(input_matrix,vertical_blocks_num)
 	for block_vert in vertical_blocks:
 		horizontal_blocks=np.hsplit(block_vert,horizontal_blocks_num)
 		for block in horizontal_blocks:
-			print block
-			print '-'*10
+			all_small_blocks.append(block)
+	return all_small_blocks,horizontal_blocks,vertical_blocks_num
 
 def test():
 	padded_table=padding_dummy_edge(test_table,8)
@@ -60,7 +61,9 @@ def test():
 	print padded_table
 	print '*'*10
 	print "cutted blocks:"
-	split_to_blocks(padded_table,4)
+	blocks,hor_num,ver_num=split_to_blocks(padded_table,4)
+	for i in blocks:
+		print i
 
 if __name__ == '__main__':
 	test()
