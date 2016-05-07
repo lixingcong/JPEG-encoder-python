796ecc2defb8de5ffcb56f266ac76e60fcac12f8
lixingcong <lixingcong@live.com>
2016-05-07 21:58:40 +0800

    [io_bmp.py]: read header

--- src/io_bmp.py
+++ src/io_bmp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Time-stamp: < io_bmp.py 2016-05-07 21:43:33 >
+# Time-stamp: < io_bmp.py 2016-05-07 21:58:13 >
 """
 读写bmp文件
 """ 
@@ -15,31 +15,38 @@ class BMP(object):
 		self.width = width
 		self.data_size = height * width
 		self.binary = binary
+		self.buffer = None
+
+	def read_header(self, f):
+		# 分辨率
+		f.seek(18)
+		self.buffer = f.read(8)
+		resolution = struct.unpack('<II', self.buffer)
+		self.width = resolution[0]
+		self.height = resolution[1]
+		self.data_size = self.width * self.height
 
 	def get_header(self):
+		return (self.height, self.width)
+		
+	def write_header(self, f):
 		pass
 
-	def write_header(self):
-		pass
+	def get_data(self):
+		return (self.height, self.width, self.binary)
 	
 	# 简易读取bmp，仅实现了灰度图像读取
 	def read_bmp(self):
-		with open(filelocation, 'rb') as f:
-			# 分辨率
-			f.seek(18)
-			buf = f.read(8)
-			size = struct.unpack('<II', buf)
-			width = size[0]
-			height = size[1]
-			data_size = width * height
+		with open(self.filelocation, 'rb') as f:
+			self.read_header(f)
 			# 数据偏移
 			f.seek(10)
-			buf = f.read(4)
-			offset_ = struct.unpack('<I', buf)
+			self.buffer = f.read(4)
+			offset_ = struct.unpack('<I', self.buffer)
 			offset = offset_[0]
 			# 读取二进制
 			f.seek(offset)
-			f.read(data_size)
+			self.binary = f.read(self.data_size)
 
 	def write_bmp(self):
 		pass
@@ -48,8 +55,11 @@ class BMP(object):
 		pass
 	
 def test():
-	pass
-	# read_bmp('data/color.bmp')
+	my_pic = BMP('data/color.bmp')
+	my_pic.read_bmp()
+	(height, width) = my_pic.get_header()
+	print height, width
+	
 
 if __name__ == '__main__':
 	test()
