4a1cc67ba4c20d72c6b062820e2e6e1160ff9f6f
lixingcong <lixingcong@live.com>
2016-05-07 09:49:29 +0800

    [zig_zag_scan.py]: support bi-direction map

--- src/zig_zag_scan.py
+++ src/zig_zag_scan.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Time-stamp: < zig_zag_scan.py 2016-05-07 09:20:07 >
+# Time-stamp: < zig_zag_scan.py 2016-05-07 09:48:52 >
 """
 Z字形的扫描,zig-zag scan
 """ 
@@ -31,27 +31,43 @@ test_table=np.array([
 	[162,162,161,161,163,158,158,158]
 ])
 
-# 整理成一个1x64的映射序列
-def generate_dict():
+# 整理成一个8x8到1x64的映射序列，可以映射正向或逆向
+def generate_dict(direction):
 	global order_of_dct_table
 	seq_dict = {}
-	for i,j in [(i,j) for i in xrange(8) for j in xrange(8)]:
-		seq_dict[order_of_dct_table[i, j]] = (i * 8 + j)
+	if direction == 'forward':
+		for i,j in [(i,j) for i in xrange(8) for j in xrange(8)]:
+			seq_dict[order_of_dct_table[i, j]] = (i * 8 + j)
+	elif direction == 'backward':	
+		for i,j in [(i,j) for i in xrange(8) for j in xrange(8)]:
+			seq_dict[i * 8 + j] = order_of_dct_table[i, j]
+	else:
+		return
 	return seq_dict
 
-def get_seq_1x64(input_matrix):
-	dict_DCT = generate_dict()
+def get_seq_1x64(input_matrix, seq_dict):
 	list_DCT = []
 	for i in xrange(64):
-		order = dict_DCT[i]
+		order = seq_dict[i]
 		list_DCT.append(input_matrix[order / 8, order % 8])
 	return list_DCT
 
-def test():
-	list_DCT = get_seq_1x64(test_table)
-	for i in list_DCT:
-		print i
+def restore_matrix_from_1x64(input_list, seq_dict):
+	output_matrix = np.zeros(64,dtype=np.uint8).reshape(8,8)
+	for i in xrange(64):
+		order = seq_dict[i]
+		output_matrix[order / 8, order % 8] = input_list[i]
+	return output_matrix
 
+def test():
+	dict_forward = generate_dict('forward')
+	list_DCT_forward = get_seq_1x64(test_table, dict_forward)
+	dict_backward = generate_dict('backward')
+	for i in xrange(64):
+		print "forward: %d->%d backward: %d->%d" % (i, dict_forward[i], i, dict_backward[i]) 
+	# np_DCT_backward = restore_matrix_from_1x64(list_DCT_forward, dict_backward)
+	# print np_DCT_backward
+	
 if __name__ == '__main__':
 	test()
 
