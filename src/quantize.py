20fa3dbd4c88fe3fc42a196bde95d2d896f05c51
lixingcong <lixingcong@live.com>
2016-05-07 16:39:03 +0800

    [quantize.py]: mistook between chrominance and luminance

--- src/quantize.py
+++ src/quantize.py
@@ -11,7 +11,7 @@ import numpy as np
 output_table=np.zeros(64,dtype=np.float64).reshape(8,8)
 
 # 色差量化矩阵，课本P125
-luminance_table=np.array([
+chrominance_table=np.array([
 	[16,11,10,16,24,40,51,61],
 	[12,12,14,19,26,58,60,55],
 	[14,13,16,24,40,57,69,56],
@@ -22,7 +22,7 @@ luminance_table=np.array([
 	[72,92,95,98,112,100,103,99]
 ])
 # 亮度量化矩阵，课本P125
-chrominance_table=np.array([
+luminance_table=np.array([
 	[17,18,24,47,99,99,99,99],
 	[18,21,26,66,99,99,99,99],
 	[24,26,56,99,99,99,99,99],
@@ -63,10 +63,10 @@ def test():
 		[-1.3, -0.4, -0.3, -1.5, -0.5, 1.7, 1.1, -0.8],
 		[-2.6, 1.6, -3.8, -1.8, 1.9, 1.2, -0.6, -0.4]
 	])
-	table_forward = get_quantisation(test_table, 'luminance', 'forward')
+	table_forward = get_quantisation(test_table, 'chrominance', 'forward')
 	print table_forward
 	print '-' * 10
-	table_backward = get_quantisation(table_forward, 'luminance', 'backward')
+	table_backward = get_quantisation(table_forward, 'chrominance', 'backward')
 	print table_backward
 	
 if __name__ == '__main__':
