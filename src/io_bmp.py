fc4193880427435e4ec38701514aa0f459a96479
lixingcong <lixingcong@live.com>
2016-05-09 21:26:11 +0800

    [io_bmp.py]: calc the real data-size

--- src/io_bmp.py
+++ src/io_bmp.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Time-stamp: < io_bmp.py 2016-05-08 01:03:33 >
+# Time-stamp: < io_bmp.py 2016-05-09 21:25:07 >
 """
 读写bmp文件
 """ 
 
-# import os
+import numpy as np
 import struct
 
 def generate_colorspace(num):
@@ -16,6 +16,12 @@ def generate_colorspace(num):
 	
 	#s = struct.pack('<{0}B'.format(4 * num), *output_list)
 	return output_list
+
+# 蔡丽梅《图像处理》P31例题2.1，计算BMP的真实二进制数据大小
+def calc_real_datasize(width, height):
+	bit_per_pixel = 8
+	w = ((width * bit_per_pixel + 31) >> 5) << 2
+	return (w * height)
 
 # Header of BMP固定格式
 HEADER_SIGN = 0x4d42 # offset 0
@@ -34,10 +40,11 @@ class BMP(object):
 		self.filelocation = filelocation
 		self.height = height
 		self.width = width
-		self.data_size = height * width
+		self.data_size = calc_real_datasize(self.width, self.height)
 		self.binary = binary
 		self.header = None
 		self.buffer = None
+		# default value, changeable
 		self.data_offset = 1078
 
 	def read_header(self, f):
@@ -48,9 +55,14 @@ class BMP(object):
 		self.width = resolution[0]
 		self.height = resolution[1]
 		self.data_size = self.width * self.height
+		# 文件数据偏置
+		f.seek(10)
+		self.buffer = f.read(4)
+		self.data_offset = struct.unpack('<I', self.buffer)[0]
+		
 
-	def get_header(self):
-		return (self.height, self.width)
+	# def get_header(self):
+	# 	return (self.height, self.width)
 		
 	def write_header(self, f):
 		self.header = struct.pack('<H', HEADER_SIGN) #offset 0
@@ -99,12 +111,6 @@ def test():
 
 	my_pic1 = BMP('/tmp/3g.bmp', height, width, bin_1)
 	my_pic1.write_bmp()
-	# my_pic.read_bmp()
-	# my_pic.write_bmp()
-	# (height, width) = my_pic.get_header()
-	# print height, width
-
-	
 
 if __name__ == '__main__':
 	test()
