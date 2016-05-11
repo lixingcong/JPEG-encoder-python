49207e95f2bd979e878d16a7c7f06714d06fbeda
lixingcong <lixingcong@live.com>
2016-05-11 23:39:20 +0800

    [zig_zag_scan.py]: define dictionary CONST

--- jpeg/zig_zag_scan.py
+++ jpeg/zig_zag_scan.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Time-stamp: < zig_zag_scan.py 2016-05-11 22:05:49 >
+# Time-stamp: < zig_zag_scan.py 2016-05-11 23:38:07 >
 """
 Z字形的扫描,zig-zag scan
 """ 
@@ -20,7 +20,7 @@ order_of_dct_table = np.array([
 	[35, 36, 48, 49, 57, 58, 62, 63],
 ])
 
-# 整理成一个8x8到1x64的映射序列，可以映射正向或逆向
+# 整理成一个8x8到1x64的映射序列，高效，可以映射正向或逆向，已经生成，无需重新生成。
 def generate_dict(direction):
 	global order_of_dct_table
 	seq_dict = {}
@@ -30,7 +30,13 @@ def generate_dict(direction):
 	else:	
 		for i,j in [(i,j) for i in xrange(8) for j in xrange(8)]:
 			seq_dict[int(i * 8 + j)] = int(order_of_dct_table[i, j])
-	return seq_dict
+	print seq_dict
+	
+# 上面的generate函数生成的字典
+scan_dict_forward={0: 0, 1: 1, 2: 8, 3: 16, 4: 9, 5: 2, 6: 3, 7: 10, 8: 17, 9: 24, 10: 32, 11: 25, 12: 18, 13: 11, 14: 4, 15: 5, 16: 12, 17: 19, 18: 26, 19: 33, 20: 40, 21: 48, 22: 41, 23: 34, 24: 27, 25: 20, 26: 13, 27: 6, 28: 7, 29: 14, 30: 21, 31: 28, 32: 35, 33: 42, 34: 49, 35: 56, 36: 57, 37: 50, 38: 43, 39: 36, 40: 29, 41: 22, 42: 15, 43: 23, 44: 30, 45: 37, 46: 44, 47: 51, 48: 58, 49: 59, 50: 52, 51: 45, 52: 38, 53: 31, 54: 39, 55: 46, 56: 53, 57: 60, 58: 61, 59: 54, 60: 47, 61: 55, 62: 62, 63: 63}
+scan_dict_backward={0: 0, 1: 1, 2: 5, 3: 6, 4: 14, 5: 15, 6: 27, 7: 28, 8: 2, 9: 4, 10: 7, 11: 13, 12: 16, 13: 26, 14: 29, 15: 42, 16: 3, 17: 8, 18: 12, 19: 17, 20: 25, 21: 30, 22: 41, 23: 43, 24: 9, 25: 11, 26: 18, 27: 24, 28: 31, 29: 40, 30: 44, 31: 53, 32: 10, 33: 19, 34: 23, 35: 32, 36: 39, 37: 45, 38: 52, 39: 54, 40: 20, 41: 22, 42: 33, 43: 38, 44: 46, 45: 51, 46: 55, 47: 60, 48: 21, 49: 34, 50: 37, 51: 47, 52: 50, 53: 56, 54: 59, 55: 61, 56: 35, 57: 36, 58: 48, 59: 49, 60: 57, 61: 58, 62: 62, 63: 63}
+
+
 
 # 把8x8的矩阵按照z字形顺序转成1x64的列表
 def get_seq_1x64(input_matrix, seq_dict):
@@ -65,17 +71,14 @@ def test():
 	print test_table
 
 	# 转成1x64
-	dict_forward = generate_dict(direction = FORWARD)
-	list_DCT_forward = get_seq_1x64(test_table, dict_forward)
+	list_DCT_forward = get_seq_1x64(test_table, scan_dict_forward)
 	print "forward: TO 64x1"
 	print list_DCT_forward
 
 	# 转成8x8
 	print "backward: TO 8x8"
-	dict_backward = generate_dict(direction = False)
-	np_DCT_backward = restore_matrix_from_1x64(list_DCT_forward, dict_backward)
+	np_DCT_backward = restore_matrix_from_1x64(list_DCT_forward, scan_dict_backward)
 	print np_DCT_backward
 	
 if __name__ == '__main__':
 	test()
-
