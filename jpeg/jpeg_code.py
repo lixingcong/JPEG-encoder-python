bd069adeb86c6c89b6575488d96afd8c36fd32f6
lixingcong <lixingcong@live.com>
2016-05-11 20:32:02 +0800

    [jpeg_code.py]: define a block encode

--- jpeg/jpeg_code.py
+++ jpeg/jpeg_code.py
@@ -5,16 +5,36 @@
 JPEG编码
 """
 
-form src import block_split
-# form src import dct
-# import quantize
-# import zig_zag_scan
-# import entropy_encode
-# import dc_ac_encode
+import block_split
+import dct
+import quantize
+import zig_zag_scan
+import dc_ac_encode
+import entropy_encode
 import numpy as np
 
+
+# z字形扫描的固定字典
+zigzig_dict_forward = zig_zag_scan.generate_dict('forward')
+zigzig_dict_backward = zig_zag_scan.generate_dict('backward')
+
 def jpeg_encode(input_matrix):
-	pass
+	temp_matrix = block_split.padding_dummy_edge(input_matrix)
+	blocks_spilted, hor_num, ver_num = block_split.split_to_blocks(temp_matrix)
+	del temp_matrix
+	last_DC_value = 0
+
+	for block in blocks_spilted:
+		DCT_table = dct.forward_dct(block - 128)
+		table_quantized = quantize.get_quantisation(DCT_table, 'chrominance', 'forward')
+		result_zig_zag = zig_zag_scan.get_seq_1x64(table_quantized, zigzig_dict_forward)
+		encoded_1 = dc_ac_encode.DC_AC_encode(result_zig_zag, last_DC_value)
+		encoded_2 = entropy_encode.get_entropy_encode(encoded_1)
+		last_DC_value = result_zig_zag[0]
+
+
+	print encoded_1
+	print encoded_2
 
 def test():
 	test_table = np.array([
@@ -27,7 +47,8 @@ def test():
 		[162, 162, 161, 163, 162, 157, 157, 157],
 		[162, 162, 161, 161, 163, 158, 158, 158]
 	])
-	pass
+	jpeg_encode(test_table)
+
 
 if __name__ == "__main__":
 	test()
