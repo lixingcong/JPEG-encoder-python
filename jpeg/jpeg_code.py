46b3b68c9c52d02736c5b34fd7e5005ed36ef740
lixingcong <lixingcong@live.com>
2016-05-13 22:28:51 +0800

    [jpeg_code.py]: decode a 8x16 jpg is ok

--- jpeg/jpeg_code.py
+++ jpeg/jpeg_code.py
@@ -19,7 +19,8 @@ from quantize import LUMINANCE
 
 
 def jpeg_encode(input_matrix):
-	output_stream = []
+	output_hex = ''
+	each_block_encoded = []
 	# 填充block长宽为8的整数
 	temp_matrix = block_split.padding_dummy_edge(input_matrix)
 	# 分割MCU
@@ -42,31 +43,30 @@ def jpeg_encode(input_matrix):
 		print encoded_1
 		# 熵编码（哈夫曼）
 		encoded_2 = entropy_encode.get_entropy_encode(encoded_1)
+		each_block_encoded.append(encoded_2)
 		# print encoded_2
-		output_stream.append(encoded_2)
+
 		# 记下当前块的DC值供下一个编号
 		last_DC_value = table_quantized[0, 0]
 
-	return output_stream
-	# TODO
-	# 二进制编码
 
-def jpeg_decode(input_list, quantize_table, width, height):
-	block_num = 1
-	last_DC_value = 0
-	restored_MCU_blocks = []
-	for MCU in input_list:
-		print "----------------\nBLOCK #%d decoding:" % block_num
-		block_num += 1
-		# 熵解码
-		decode_1 = entropy_encode.get_entropy_decode(MCU)
-		print decode_1
+	# 二进制编码
+	output_hex = entropy_encode.get_encoded_to_hex(each_block_encoded)
+	return output_hex
+
+def jpeg_decode(input_hex, quantize_table, width, height):
+	decoded_blocks = []
+	previous_DC_value = 0
+	# 熵解码
+	entropy_decoded_bin = entropy_encode.get_decoded_from_hex(input_hex, is_debug = False)
+	entropy_decoded_blocks = entropy_encode.get_entropy_decode(entropy_decoded_bin)
+	# 对每一个MCU块进行解码
+	for block in entropy_decoded_blocks:
 		# DC AC解码
-		decode_2 = dc_ac_encode.DC_AC_decode(decode_1, last_DC_value)
-		print decode_2
-		# zig-zag还原成8x8矩阵
-		result_zig_zag = zig_zag_scan.restore_matrix_from_1x64(decode_2)
-		print result_zig_zag
+		decoded_dc_ac = dc_ac_encode.DC_AC_decode(block, previous_DC_value)
+				# zig-zag还原成8x8矩阵
+		result_zig_zag = zig_zag_scan.restore_matrix_from_1x64(decoded_dc_ac)
+
 		# 反量化
 		table_unquantized = quantize.get_quantisation(result_zig_zag, quantize_table, False)
 		# 逆DCT变换
@@ -79,23 +79,26 @@ def jpeg_decode(input_list, quantize_table, width, height):
 				IDCT_table[i, j] = 127
 			else:
 				IDCT_table[i, j] = -128
-		restored_MCU_blocks.append(IDCT_table + 128)
+		decoded_blocks.append(IDCT_table + 128)
 		# 更新DC值
-		last_DC_value = result_zig_zag[0, 0]
+		previous_DC_value = result_zig_zag[0, 0]
+
 	# 合并前计算padded尺寸
 	width_padded, height_padded = block_split.calc_new_size(width, height)
 	# 计算MCU的行列数
 	columns, rows = width_padded / 8, height_padded / 8
 	# 合并
-	merged_matrix = block_split.merge_blocks(restored_MCU_blocks, rows, columns)
+	merged_matrix = block_split.merge_blocks(decoded_blocks, rows, columns)
 	# 移除多余的边缘像素
 	final_matrix = block_split.remove_dummy_edge(merged_matrix, width, height)
 	return final_matrix
 
+
+
 def test():
 	q_table = np.ones(64, dtype = np.uint8).reshape(8, 8)
-	test_list = [[('1111110', '101010011'), ('11111000', '1000010'), ('1111110110', '10011101'), ('1011', '1011'), ('11111000', '0101000'), ('11111000', '1010000'), ('11010', '01000'), ('1111110110', '10111110'), ('11111000', '1101000'), ('1111110110', '10011111'), ('1111000', '110100'), ('100', '110'), ('1111110110', '01010011'), ('11111000', '0000011'), ('1111110110', '01100000'), ('1111110110', '01111010'), ('100', '010'), ('11111000', '1000010'), ('1111000', '010110'), ('1111000', '110000'), ('1011', '1111'), ('11010', '11011'), ('1111000', '100010'), ('1111000', '011101'), ('1011', '0011'), ('1111000', '110101'), ('1011', '0001'), ('100', '100'), ('1011', '0010'), ('1111000', '001000'), ('11111000', '1010100'), ('1111000', '000101'), ('1111000', '110000'), ('11111000', '0101100'), ('11010', '11001'), ('1011', '0011'), ('1111000', '100110'), ('00', '0'), ('11111000', '1000000'), ('11111000', '1001011'), ('1111000', '110010'), ('1111000', '110101'), ('1111000', '100101'), ('1111000', '011001'), ('01', '11'), ('1011', '1110'), ('11111000', '1011100'), ('1111000', '001100'), ('1111000', '001010'), ('1111000', '100000'), ('11111000', '1000011'), ('11010', '00001'), ('1011', '1101'), ('1111000', '110100'), ('1111000', '010101'), ('1011', '1100'), ('11010', '10001'), ('1111000', '110001'), ('11010', '00000'), ('11111000', '0110011'), ('11010', '01110'), ('01', '11'), ('100', '101'), ('1111000', '101011')]]
-	print jpeg_decode(test_list, q_table, 8, 8)
+	test_hex = 'fe8d7e1a7c00b1f08f87f4cf0d6950de49a6e9ad78d6df6e9daeae337d7f77a8cfbe6645dc05cde4c22508ab1c4238d46d415f23ffc161ffe0a59f02bf663fd907c73e18f827f147c3fe2ff8f7f16daffe19f82d7c1c1fc45a6f87b4c17b0e9df13bc4d75e23b30ba0c573a36832df785f458f4cd5af75eb7f1bebba36a10e95268de1ff156a5a17'
+	print jpeg_decode(test_hex, q_table, 8, 16)
 	exit(0)
 	# 测试矩阵，在课本P129
 	test_table = np.array([
@@ -115,7 +118,7 @@ def test():
 	encoded = jpeg_encode(test_table)
 	print  '#' * 10 + "\nencoded:"
 	print encoded
-
+	exit(0)
 	# 解码
 	decoded = jpeg_decode(encoded, 8, 9)
 	print '#' * 10 + "\ndecoded:"
